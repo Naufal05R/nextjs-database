66284ac8e8e2cf80393f49784a674004eb6b906a
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-09 12:03:29 +0700

    feat: Analyzing a new server action

---
+++ lib/products.ts
@@ -0,0 +1,8 @@
+"use server";
+
+// import our generated Prisma Client
+import { prisma } from "@/lib/prisma";
+
+export async function createProduct() {
+  // TODO: create a new Product in the database
+}
