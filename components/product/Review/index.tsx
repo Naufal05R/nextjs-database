32aab3b2b2707ffcbef43b8956e36540a04e42bb
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-10 18:46:37 +0700

    feat: adding dynamic alt name

--- components/product/Review/index.tsx
+++ components/product/Review/index.tsx
@@ -8,14 +8,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 export default function ReviewView({ review }: { review: Review }) {
   const splittedName = review.name.split(" ");
   const initials = splittedName.map((name) => name[0]).join("");
-  const altName = splittedName.join("");
+  const altName = splittedName.join("").toLowerCase();
 
   return (
     <Card>
       <CardContent className="grid gap-4 p-4">
         <div className="flex items-center gap-4">
           <Avatar>
-            <AvatarImage alt="@jaredpalmer" src="/placeholder-avatar.jpg" />
+            <AvatarImage alt={`@${altName}`} src="/placeholder-avatar.jpg" />
             <AvatarFallback>{initials}</AvatarFallback>
           </Avatar>
           <div>
