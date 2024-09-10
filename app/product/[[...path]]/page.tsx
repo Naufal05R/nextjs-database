eb9de8d8bd7cd5dcec94bbfb406fbb13ba02a513
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-10 18:38:56 +0700

    feat: passing id to the AddReview component

--- app/product/[[...path]]/page.tsx
+++ app/product/[[...path]]/page.tsx
@@ -39,7 +39,7 @@ export default async function Page({ params }: { params: { path: string[] } }) {
         </div>
       </div>
       <div className="md:col-span-2">
-        <AddReview />
+        <AddReview id={id} />
       </div>
     </div>
   );
