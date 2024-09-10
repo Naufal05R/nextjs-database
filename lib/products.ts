8d3e3418a4642d3de6b7859058837aa3d5330a13
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-10 14:20:48 +0700

    feat: create a new Interface for params type

--- lib/products.ts
+++ lib/products.ts
@@ -3,6 +3,13 @@
 // import our generated Prisma Client
 import { prisma } from "@/lib/prisma";
 
+interface CreateProductInput {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+}
+
 export async function createProduct() {
   // TODO: create a new Product in the database
 }
