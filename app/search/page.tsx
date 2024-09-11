f31ea52e406dc60a22b93f07ab16ff56059b79d0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-11 21:15:51 +0700

    feat: adding new props and passing params to getProducts

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,8 +2,18 @@ import ProductResult from "@/components/search/ProductResult";
 import SearchFilters from "@/components/search/SearchFilters";
 import { getProducts } from "@/lib/actions/products";
 
-export default async function Page() {
-  const products = await getProducts();
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    name: string;
+    category: string;
+    minRating: string;
+    page: string;
+  };
+}) {
+  const page = parseInt(searchParams.page);
+  const products = await getProducts({ page });
 
   return (
     <div className="grid md:grid-cols-[300px_1fr] gap-8 px-4 md:px-8 py-20">
