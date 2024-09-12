4faaa1fa2a9b2bd9c0b2db346051a3ca256f662e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-12 18:45:37 +0700

    feat: implement rating filter to searchParams

--- components/search/filterComponents/Rating.tsx
+++ components/search/filterComponents/Rating.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import RatingSelect from "@/components/product/Review/RatingSelect";
 import { Label } from "@/components/ui/label";
 import { useURLQuery } from "@/lib/hooks/useURLQuery";
 
 export default function Rating() {
-  const [rating, setRating] = useURLQuery("rating", "", 200);
+  const { 1: setRating } = useURLQuery("rating", "");
   return (
     <div className="flex flex-col">
       <Label className="text-base" htmlFor="rating-filter">
         Rating
       </Label>
+      <RatingSelect onChange={(s: number) => setRating(String(s || ""))} />
     </div>
   );
 }
