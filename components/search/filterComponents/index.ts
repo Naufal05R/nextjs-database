c3ca2b39e6cde320816765d2965d4fec1bc5ddb1
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-12 18:29:17 +0700

    feat: adding Rating to the index

--- components/search/filterComponents/index.ts
+++ components/search/filterComponents/index.ts
@@ -1,3 +1,4 @@
 export { default as Category } from "./Category";
 export { default as Name } from "./Name";
 export { default as Price } from "./Price";
+export { default as Rating } from "./Rating";
