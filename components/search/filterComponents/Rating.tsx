851630285e51ba4a7c4590695eb7f5e0cb7d67e9
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-12 18:28:13 +0700

    feat: making functional component returning an element

--- components/search/filterComponents/Rating.tsx
+++ components/search/filterComponents/Rating.tsx
@@ -4,4 +4,9 @@ import { useURLQuery } from "@/lib/hooks/useURLQuery";
 
 export default function Rating() {
   const [rating, setRating] = useURLQuery("rating", "", 200);
+  return (
+    <div className="flex flex-col">
+      
+    </div>
+  );
 }
