281d1015fb9321e7953f687e7b27cfa5190e4adf
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-12 10:09:52 +0700

    feat: modifying searchParams and getting page from searchParams

--- components/search/Pagination.tsx
+++ components/search/Pagination.tsx
@@ -5,8 +5,10 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { modifySearchParams } from "@/lib/utils";
 
 const Pagination = () => {
-  const searchParams = useSearchParams();
   const router = useRouter();
+  const searchParams = Object.fromEntries(useSearchParams());
+
+  const page = parseInt(searchParams.page) || 1;
 
   const handlePageChange = (newPage: number) => {
     
