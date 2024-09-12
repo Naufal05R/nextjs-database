89a12be15b8ab003333805c28e4e64de2e8c1d71
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-12 09:25:38 +0700

    feat: initialing searchParams & router

--- components/search/Pagination.tsx
+++ components/search/Pagination.tsx
@@ -3,3 +3,12 @@
 import { useRouter, useSearchParams } from "next/navigation";
 
 import { modifySearchParams } from "@/lib/utils";
+
+const Pagination = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  return <div></div>;
+};
+
+export default Pagination;
