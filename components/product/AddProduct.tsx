b6c1702256f686f42852035c314dfeae34dce510
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-12 16:53:25 +0700

    feat: passing initialImages

--- components/product/AddProduct.tsx
+++ components/product/AddProduct.tsx
@@ -139,7 +139,10 @@ export default function AddProduct({
               rows={5}
             />
           </div>
-          <ImageSelect onChange={(value) => setImages(value)} />
+          <ImageSelect
+            initialImages={images}
+            onChange={(value) => setImages(value)}
+          />
         </div>
         <div className="flex justify-end gap-2">
           <Button variant="outline">Cancel</Button>
