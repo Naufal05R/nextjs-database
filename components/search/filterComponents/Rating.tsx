95a691f67037b194376020d04908d7fa69b1f2f5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-12 18:28:28 +0700

    feat: adding Label

--- components/search/filterComponents/Rating.tsx
+++ components/search/filterComponents/Rating.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Label } from "@/components/ui/label";
 import { useURLQuery } from "@/lib/hooks/useURLQuery";
 
 export default function Rating() {
   const [rating, setRating] = useURLQuery("rating", "", 200);
   return (
     <div className="flex flex-col">
-      
+      <Label className="text-base" htmlFor="rating-filter">
+        Rating
+      </Label>
     </div>
   );
 }
