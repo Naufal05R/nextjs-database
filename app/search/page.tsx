ef7b43471def1e4d658ddeccf38676377120c321
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-12 11:17:29 +0700

    feat: adding Pagination component to the / route page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,4 @@
+import Pagination from "@/components/search/Pagination";
 import ProductResult from "@/components/search/ProductResult";
 import SearchFilters from "@/components/search/SearchFilters";
 import { getProducts } from "@/lib/actions/products";
@@ -12,13 +13,14 @@ export default async function Page({
     page: string;
   };
 }) {
-  const page = parseInt(searchParams.page);
+  const page = parseInt(searchParams.page) || 1;
   const products = await getProducts({ page });
 
   return (
     <div className="grid md:grid-cols-[300px_1fr] gap-8 px-4 md:px-8 py-20">
       <div className="bg-white rounded-lg shadow-sm dark:bg-gray-950 p-6 space-y-6">
         <SearchFilters />
+        <Pagination />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product) => (
