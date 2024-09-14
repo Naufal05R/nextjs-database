d2df50f25189d102056af0c015afa07a1d36b399
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-14 22:54:31 +0700

    fix: adding return to the component

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,6 +5,7 @@ import ProductResult from "@/components/search/ProductResult";
 import SearchFilters from "@/components/search/SearchFilters";
 import { getProducts } from "@/lib/actions/products";
 import Stars from "@/components/product/Stars";
+import { ProductListSkeleton } from "@/components/ui/skeletons";
 
 export default function Page({
   searchParams,
@@ -25,36 +26,7 @@ export default function Page({
         <SearchFilters />
         <Pagination />
       </div>
-      <Suspense
-        key={page}
-        fallback={
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 min-w-64 lg:grid-cols-2 xl:grid-cols-3 gap-6">
-            {Array.from({ length: 12 }).map((_, index) => (
-              <div
-                key={index}
-                className="bg-white rounded-lg shadow-sm dark:bg-gray-950 overflow-hidden"
-              >
-                <div className="block">
-                  <img
-                    src="https://dummyimage.com/600x520/000/fff"
-                    alt="product"
-                    className="w-full aspect-video object-cover"
-                  />
-                  <div className="p-4 space-y-2">
-                    <h3 className="font-semibold text-lg">Product Name</h3>
-                    <div className="flex items-center gap-1">
-                      <Stars rating={Math.round(4.5)} />
-                      <span className="text-sm text-gray-500 dark:text-gray-400 ml-2">
-                        4.5 out of 2
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        }
-      >
+      <Suspense key={page} fallback={<ProductListSkeleton length={12} />}>
         <ProductList {...searchParams} page={page} />
       </Suspense>
     </div>
